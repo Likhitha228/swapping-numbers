9e2244718d26f392fbba76c2c5c806c7a8946160
Likhitha228 <90981048+Likhitha228@users.noreply.github.com>
2021-09-19 14:45:07 +0530

    Update Swap.c

--- Swap.c
+++ Swap.c
@@ -17,3 +17,15 @@ int main()
 	printf("After swapping....a:%d,b:%d\n",a,b);
 	return 0;
 }
+
+Output
+
+Enter the value of a:10
+Enter the value of b:20
+Before swapping....a:10,b:20
+After swapping....a:20,b:10
+
+Enter the value of a:-100
+Enter the value of b:-200
+Before swapping....a:-100,b:-200
+After swapping....a:-200,b:-100
